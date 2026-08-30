14912e3a242e9a398fc11f248a84e71a4ad27325
Jamaal-M04 <jamaalmohammed400@gmail.com>
2026-08-30 17:38:38 +0100

    update

--- pythonprojects/rockpaper.py
+++ pythonprojects/rockpaper.py
@@ -3,15 +3,19 @@ choices = ["rock", "paper", "scissors"]
 
 computer_choice =random.choice(choices)
 
-your_choice = input("selct from; rock, paper, scissors")
+your_choice = input("selct from; rock, paper, scissors").lower()
+while your_choice not in choices:
+    print("try again")
+    your_choice = input("selct from; rock, paper, scissors").lower()
+
 
 if your_choice == computer_choice:
     print("It's a draw!")
 elif your_choice == "rock" and computer_choice == "scissors":
-    print("Player wins")
+    print(f"Player wins, computer chose {computer_choice}")
 elif your_choice == "paper" and computer_choice == "rock":
-    print("Player wins")
+    print(f"Player wins, computer chose {computer_choice}")
 elif your_choice == "scissors" and computer_choice == "paper":
-    print("Player wins")
+    print(f"Player wins, computer chose {computer_choice}")
 else:
-    print("Player loses")
+    print(f"Player loses, computer chose {computer_choice}")
