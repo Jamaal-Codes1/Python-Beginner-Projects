9e0ff17944db65895e17d854d06932f0e0d0d452
Jamaal-M04 <jamaalmohammed400@gmail.com>
2026-08-30 16:20:32 +0100

    updated

--- pythonprojects/rockpaper.py
+++ pythonprojects/rockpaper.py
@@ -2,3 +2,8 @@ import random
 choices = ["rock", "paper", "scissors"]
 
 computer_choice =random.choice(choices)
+
+your_choice = input("selct from; rock, paper, scissors")
+
+if your_choice == computer_choice:
+    print("It's a draw!")
