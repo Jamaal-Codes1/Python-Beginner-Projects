dabcf35091af49cfcee2cea483a168197dfef0b7
Jamaal-M04 <jamaalmohammed400@gmail.com>
2026-08-30 16:32:30 +0100

    updated

--- pythonprojects/rockpaper.py
+++ pythonprojects/rockpaper.py
@@ -7,3 +7,7 @@ your_choice = input("selct from; rock, paper, scissors")
 
 if your_choice == computer_choice:
     print("It's a draw!")
+elif your_choice == "rock" and computer_choice == "scissors":
+    print("Player wins")
+else:
+    print("Player loses")
