c95db0a239976d9c2bbe381d68d122dda50ead3b
Jamaal-M04 <jamaalmohammed400@gmail.com>
2026-08-31 14:27:39 +0100

    update

--- pythonprojects/rockpaper.py
+++ pythonprojects/rockpaper.py
@@ -6,24 +6,25 @@ computer_choice =random.choice(choices)
 player_score = 0
 computer_score = 0
 
-
-your_choice = input("selct from; rock, paper, scissors").lower()
-while your_choice not in choices:
-    print("try again")
+while player_score <2 and computer_score <2:
+        
     your_choice = input("selct from; rock, paper, scissors").lower()
+    while your_choice not in choices:
+        print("try again")
+        your_choice = input("selct from; rock, paper, scissors").lower()
 
 
-if your_choice == computer_choice:
-    print("It's a draw!")
-elif your_choice == "rock" and computer_choice == "scissors":
-    print(f"Player won round, computer chose {computer_choice}")
-    player_score += 1
-elif your_choice == "paper" and computer_choice == "rock":
-    print(f"Player won round, computer chose {computer_choice}")
-    player_score += 1
-elif your_choice == "scissors" and computer_choice == "paper":
-    print(f"Player won round, computer chose {computer_choice}")
-    player_score += 1
-else:
-    print(f"Player loses, computer chose {computer_choice}")
-    computer_score += 1
+    if your_choice == computer_choice:
+        print("It's a draw!")
+    elif your_choice == "rock" and computer_choice == "scissors":
+        print(f"Player won round, computer chose {computer_choice}")
+        player_score += 1
+    elif your_choice == "paper" and computer_choice == "rock":
+        print(f"Player won round, computer chose {computer_choice}")
+        player_score += 1
+    elif your_choice == "scissors" and computer_choice == "paper":
+        print(f"Player won round, computer chose {computer_choice}")
+        player_score += 1
+    else:
+        print(f"Player loses, computer chose {computer_choice}")
+        computer_score += 1
