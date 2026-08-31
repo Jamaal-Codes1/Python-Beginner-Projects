b7aaec5100fed810f13ea245a613e6acd5687150
Jamaal-M04 <jamaalmohammed400@gmail.com>
2026-08-31 15:40:17 +0100

    updae

--- pythonprojects/Todolist.py
+++ pythonprojects/Todolist.py
@@ -1,22 +1,30 @@
-
 tasks = []
-
-print("================================")
-print("TO-DO LIST")
-print("================================")
-
-print("Welcome back Jamaal,")
-print(f"You currently have {len(tasks)} tasks in your to do list. ")
+option = 0
+while option != 4:
 
 
-print("1: Set a Task")
-print("2: view your Tasks")
-print("3: Remove your Task")
-print("4: Exit")
+    print("================================")
+    print("TO-DO LIST")
+    print("================================")
 
-option = int(input("Select your option"))
+    print("Welcome back Jamaal,")
+    print(f"You currently have {len(tasks)} tasks in your to do list. ")
 
-if option == 1:
-    task = input("set a task")
-    tasks.append(task)
-    print(f"Task added successfully {task}")
+
+    print("1: Set a Task")
+    print("2: view your Tasks")
+    print("3: Remove your Task")
+    print("4: Exit")
+
+    option = int(input("Select your option"))
+
+    if option == 1:
+        task = input("set a task")
+        tasks.append(task)
+        print(f"Task added successfully {task}")
+    elif option == 2:
+        if not tasks:
+            print("You have no tasks")
+        else:
+            for index, task in enumerate(tasks, start=1):
+                print(f"{index}. {task}")
